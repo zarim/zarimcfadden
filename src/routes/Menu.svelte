927617525e8c17ee39177ad8f43a2c63dc3c2455
Zari McFadden <zmuhammad55@gmail.com>
2024-10-12 21:06:46 -0400

    Revert "adding writings menu item and content"
    This reverts commit e0dfd1223ff2bfcee02fb801aee3048e5b849046.

--- src/routes/Menu.svelte
+++ src/routes/Menu.svelte
@@ -7,25 +7,16 @@
         <a sveltekit:prefetch href="/" style="color: #323649;">About</a>
         <a sveltekit:prefetch href="/WebWork" >Work</a>
         <a sveltekit:prefetch href="/Resume">Resume</a>
-        <a sveltekit:prefetch href="/Writings">Writings</a>
     {/if}
     {#if page == "work" }
         <a sveltekit:prefetch href="/">About</a>
         <a sveltekit:prefetch href="/WebWork" style="color: #323649;">Work</a>
         <a sveltekit:prefetch href="/Resume">Resume</a>
-        <a sveltekit:prefetch href="/Writings">Writings</a>
     {/if}
     {#if page == "resume" }
         <a sveltekit:prefetch href="/">About</a>
         <a sveltekit:prefetch href="/WebWork" >Work</a>
         <a sveltekit:prefetch href="/Resume" style="color: #323649;">Resume</a>
-        <a sveltekit:prefetch href="/Writings">Writings</a>
-    {/if}
-    {#if page == "writings" }
-        <a sveltekit:prefetch href="/">About</a>
-        <a sveltekit:prefetch href="/WebWork" >Work</a>
-        <a sveltekit:prefetch href="/Resume">Resume</a>
-        <a sveltekit:prefetch href="/Writings" style="color: #323649;">Writings</a>
     {/if}
 </div>
 
@@ -43,7 +34,7 @@
         font-size: 20px;
         font-weight: 300;
         margin: auto;
-        width: 30%;
+        width: 20%;
         padding-bottom: 50px;
     }
 
